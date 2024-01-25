c1b04426c24a7451bc848c3aed2f0ff4929e9079
hashinivish <e19142@eng.pdn.ac.lk>
2024-01-25 22:53:55 +0530

    EmployeePage

--- src/Pages/EmployeePage.js
+++ src/Pages/EmployeePage.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Heading, Image, View, Card, Button } from "@aws-amplify/ui-react";
-import logo from './../img/PALLETPRO.png';
+import { Heading, Image, View, Card, Button, Grid, useTheme, Flex } from "@aws-amplify/ui-react";
+import logo from './../img/LOGO.png';
+import map from './../img/Map.png';
 
-const EmployeePage = ({ signOut }) => (
-  <View>
-    <Heading level={1}>Welcome to Employee Page!</Heading>
-    <Image src={logo} />
-    {/* Employee-specific content */}
-    <Card>
-      <Button onClick={signOut}>Sign out</Button>
+
+const EmployeePage = ({ signOut }) => {
+  const { tokens } = useTheme();
+
+  return (
+    <Grid
+    style={{
+      height: '100vh',
+      width: '100vw',
+    }}
+    // columnGap="0.5rem"
+    // rowGap="0.5rem"
+    templateColumns="2fr 1fr"
+    templateRows="1fr 7fr"
+  >
+    <Card
+      columnStart="1"
+      columnEnd="-1"
+      backgroundColor="#140d07"
+      width="100%"
+      height="100%"
+    >
+      <Flex alignItems="center" justifyContent="space-between">
+            <Image src={logo} width="6%" />
+          <Flex>
+          <Button colorTheme="info" variation="default" backgroundColor="#4fb9af"  >
+          Request a Pallet
+          </Button>
+          <Button colorTheme="info" variation="default" backgroundColor="#4fb9af" onClick={signOut}>Sign out</Button>
+          </Flex>
+        </Flex>
     </Card>
-  </View>
-);
+    <Card
+      columnStart="1"
+      columnEnd="2"
+      backgroundColor="#140d07"
+    >
+     <Image src={map}/>
+    </Card>
+    <Card
+      columnStart="2"
+      columnEnd="-1"
+      backgroundColor="#140d07"
+    >
+      Main
+    </Card>
+  </Grid>
+  );
+  };
 
 export default EmployeePage;
