d5331a664707fbd880f9982cfdf3c19e6a65ee96
hashinivish <e19142@eng.pdn.ac.lk>
2024-01-31 11:21:01 +0530

    signOutAdded

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
 import { Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
 import logo from './../img/PALLETPRO.png';
+import { signOut } from 'aws-amplify/auth';
 
 const LandingPage = () => (
   <View backgroundColor="#140d07">
@@ -10,6 +11,7 @@ const LandingPage = () => (
     <Card>
       
       <Button onClick={Authenticator}>Sign In/Up</Button>
+      <button onClick={signOut}>Sign Out</button>
     </Card>
   </View>
 );
